41ad86155aa29b5aa03f563af9d4592819cd3b35
sandhra-royston <137055589+sandhra-royston@users.noreply.github.com>
2024-06-03 20:26:11 +0530

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -3,4 +3,4 @@ cd /home/ubuntu/django_demo || exit
 # Activate the virtual environment
 source venv/bin/activate
 
-python manage.py runserver 0.0.0.0:8000
+nohup python manage.py runserver 0.0.0.0:8000
